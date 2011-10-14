f0798f580dfb8fef4a7ad7d40556baffadc2538d
User <uchida@re-inc.jp>
2011-10-14 15:35:02 +0900

    ミッドナイトメール更新

--- cron/midNightMail.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /home/re-dev/www/demo/demo2/hotel1/cron
-
-/usr/local/bin/php midnight_mail.php >> mail.log
-
--- cron/midnight_mail.php
+++ cron/midnight_mail.php
@@ -3,21 +3,26 @@ define("DEBUG", "yes");
 	require_once("../admin/inc/include_class_files.php");
 	include_once("../admin/inc/dbcon.inc.php");
 
+	$obj = new dbo;
+	
+	include("../admin/inc/main_dbcon.inc.php");
+	$hcode=$HOTELID;
+	$hotel_name = $obj->GetSingleData(" super_spssp_hotel ", " hotel_name ", " hotel_code=".$hcode);
+	include("../admin/inc/return_dbcon.inc.php");	
+	
 	$objMsg = new MessageClass();
 
-//	$query_string="SELECT * FROM spssp_user where party_day >= '".date("Y-m-d")."' and mail != '' and subcription_mail=0;";
 	$query_string="SELECT * FROM spssp_user where party_day >= '".date("Y-m-d")."' order by party_day asc;";
 	$data_rows=mysql_query($query_string);
 
-	if (DEBUG!=NULL) echo "ミッドナイトメール　：　".date("Y-m-d")."<br />\n";
+	if (DEBUG!=NULL) echo "ミッドナイトメール　：　".$hotel_name." :  ID=".$HOTELID." : ".date("Y-m-d")."<br />\n";
 	if (DEBUG!=NULL) echo "-------------------------------------<br />\n";
 	while($row=mysql_fetch_array($data_rows))
 	{
  		$ret = $objMsg->admin_side_user_list_new_status_notification_image_link_system($row['id'], DEBUG);
 		if ($ret != NULL && DEBUG!=NULL) echo $row['id']." : ".$row['party_day']." : ".$row['man_lastname']." : ".$row['woman_lastname']." : ".$row['mail']." : ".$ret."<br />\n";
-//		if (DEBUG!=NULL) echo "席次表：　".$row['id']." : ".$row['party_day']." : ".$row['man_lastname']." : ".$row['woman_lastname']." : ".$row['mail']." : ".$ret."<br />\n";
 		$ret = $objMsg->admin_side_user_list_gift_day_limit_notification_image_link_system($row['id'], DEBUG);
 		if ($ret != NULL && DEBUG!=NULL) echo $row['id']." : ".$row['party_day']." : ".$row['man_lastname']." : ".$row['woman_lastname']." : ".$row['mail']." : ".$ret."<br />\n";
-//		if (DEBUG!=NULL) echo "引出物：　".$row['id']." : ".$row['party_day']." : ".$row['man_lastname']." : ".$row['woman_lastname']." : ".$row['mail']." : ".$ret."<br />\n<br />\n";
 	}
+	if (DEBUG!=NULL) echo "<br />\n";
 ?>
