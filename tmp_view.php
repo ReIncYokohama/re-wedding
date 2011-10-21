15f4bda5238b3ac4a9c5a50238a2197738ea74b4
rei kubonaga <kubonagarei@gmail.com>
2011-10-21 18:14:02 +0900

    JIS90異形文字が●にしてアップロード

--- tmp_view.php
+++ tmp_view.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
-include_once("inc/gaiji.image.wedding.php");
+require("inc/gaiji.image.wedding.php");
 /*
 class Gaiji(){
   
@@ -14,5 +14,6 @@ $respect = $_GET["respect"];
 
 //make_user_tategaki_view("新郎",$first_name);
 //make_name_plate_view($last_name,$first_name,$comment1,$comment2,array("gaiji/upload/img_ans/FAB1.png","gaiji/upload/img_ans/FAB1.png"),array(),array(),array(),array(0x00,0x00,0x00),$respect);
-make_text_view($last_name,array("gaiji/upload/img_ans/FAB1.png"));
+//make_text_view($last_name,array("gaiji/upload/img_ans/FAB1.png"));
+print check_sjis($last_name);
 
