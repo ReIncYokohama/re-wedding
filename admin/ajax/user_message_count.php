f21220b88acb5382fc746b067376e1adc9d17a4b
User <uchida@re-inc.jp>
2011-10-28 19:35:23 +0900

    admin/ajax 不要ファイル削除

--- admin/ajax/user_message_count.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once("../inc/class.dbo.php");
-	include_once("../inc/checklogin.inc.php");
-
-	$obj = new DBO();
-	$num=$obj->GetNumRows("spssp_message", " user_id=".(int)$_POST['id']);
-	echo $num+1;	
-?>
