b3bbb2fdb2f00365798a7caa3a8e4f0784222729
rei kubonaga <kubonagarei@gmail.com>
2011-11-09 17:54:14 +0900

    #185 引き出物プレビュー用の画像を作成

--- tmp_view.php
+++ tmp_view.php
@@ -11,9 +11,12 @@ $first_name = $_GET["first_name"];
 $comment1 = $_GET["comment1"];
 $comment2 = $_GET["comment2"];
 $respect = $_GET["respect"];
+$memo1 = $_GET["memo1"];
+$memo2 = $_GET["memo2"];
+$memo3 = $_GET["memo3"];
 
 //make_user_tategaki_view("新郎",$first_name);
 //make_name_plate_view($last_name,$first_name,$comment1,$comment2,array("gaiji/upload/img_ans/FAB1.png","gaiji/upload/img_ans/FAB1.png"),array(),array(),array(),array(0x00,0x00,0x00),$respect);
 //make_text_view($last_name,array("gaiji/upload/img_ans/FAB1.png"));
-print check_sjis($last_name);
-
+//print check_sjis($last_name);
+make_name_plate_full_view($last_name,$first_name,"","",$memo1,$memo2,$memo3,array(),array(),array(),array(),array(0x00,0x00,0x00),"様");
