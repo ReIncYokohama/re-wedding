0c8399410734332ffcbaadf8bf797bbac7882182
User <uchida@re-inc.jp>
2011-11-11 11:49:38 +0900

    #131 印刷イメージの確認のボタン

--- make_plan.php
+++ make_plan.php
@@ -350,7 +350,7 @@ height:30px;
 			  <table width="800" border="0" cellspacing="1" cellpadding="3">
 				  <tr>
 				    <?php 
-				    if ($obj->GetRowCount("spssp_plan"," admin_to_pcompany = 2 and `ul_print_com_times` < 2 and `order` = 1 and user_id=".$user_id) && ($plan_info['dl_print_com_times'] & 0x100) == 0x00) {
+				    if ($obj->GetRowCount("spssp_plan"," admin_to_pcompany = 2 and `ul_print_com_times` < 2 and `order` = 1 and user_id=".$user_id)) {
 				    ?>
 				    	<td width="210" valign="middle"><a href="<?=substr($plan_info['p_company_file_up'], 3)?>" target="_blank"><img src="img/order/preview_print_bt.jpg" alt="席次表プレビュー" width="200" height="40" border="0" class="on"/></a></td>
 				    <?php 
