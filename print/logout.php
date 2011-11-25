d2db833574ffd23c126a997aa1de81673a4913f3
User <uchida@re-inc.jp>
2011-11-25 19:15:53 +0900

    #212 FireFoxだけメッセージ表示

--- print/logout.php
+++ print/logout.php
@@ -6,6 +6,12 @@ unset($_SESSION['printid']);
 unset($_SESSION['contact_name']);
 //header("Location:index.php");
 
+if (mb_ereg("Firefox", getenv( "HTTP_USER_AGENT" ))) {
+	echo "<script type='text/javascript'>";
+	echo "alert('ログアウトしました。閉じるボタン等でウインドウを閉じてださい')";
+	echo "</script>";
+}
+
 echo "<script type='text/javascript'>";
 echo " window.open('about:blank','_self').close();";
 echo "window.opener = '';";
