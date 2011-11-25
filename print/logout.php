e33e2abaf9fdcd5f7641f32d6125e166a28e4b11
User <uchida@re-inc.jp>
2011-11-25 16:50:32 +0900

    #212 印刷会社ログアウト対応：ブラウザにより機能が異なるが対応

--- print/logout.php
+++ print/logout.php
@@ -5,7 +5,11 @@ unset($_SESSION['email']);
 unset($_SESSION['printid']);
 unset($_SESSION['contact_name']);
 //header("Location:index.php");
+
+echo "<script type='text/javascript'>";
+echo " window.open('about:blank','_self').close();";
+echo "window.opener = '';";
+echo "window.self.close();";
+echo "</script>";
+return;
 ?>
-<script>
-window.close();
-</script>
