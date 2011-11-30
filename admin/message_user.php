1c21c7ccb7c0ecb8b45bafbb44c7ff7c4d6f1a2a
User <uchida@re-inc.jp>
2011-11-30 19:46:04 +0900

    #250

--- admin/message_user.php
+++ admin/message_user.php
@@ -159,7 +159,8 @@ function m_win(url,windowname,width,height) {
   else height=window.screen.height;
   features+=", height="+height;
  }
- window.open(url,windowname,features);
+// window.open(url,windowname,features);
+ window.showModalDialog(url,windowname,"dialogTop:400px; dialogLeft:600px; dialogwidth:"+width+"px; dialogheight:"+height+"px;");
 }
 // --></script>
 <div id="topnavi">
