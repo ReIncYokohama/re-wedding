2e5f5c7d0f7f1dc2bb3cc6152978441d00b3ddef
User <uchida@re-inc.jp>
2011-12-06 20:41:39 +0900

    #248 ホ/ユ：姓名敬称を編集した時の名前画像を自動リフレッシュする

--- load_guest_image.php
+++ load_guest_image.php
@@ -12,6 +12,7 @@ $message = $_GET["message"];
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <script language="javascript" type="text/javascript" src="js/jquery.js"></script>
 </head>
 <body>
