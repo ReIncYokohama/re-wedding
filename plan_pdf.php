313aa5e464457f936c1c9271bb453078c3f28ca8
User <uchida@re-inc.jp>
2011-12-09 13:53:41 +0900

    #268 引き出物プレビューの右揃え、席次表の肩書き１と２の結合

--- plan_pdf.php
+++ plan_pdf.php
@@ -595,7 +595,7 @@ foreach($tblrows as $tblrow)
                       {
                         $middle_string="";
 								
-                        $middle_string .= $objInfo->get_user_name_image_or_src_from_user_side($user_id ,$hotel_id=1, $name="namecard_memo2.png",$extra="guest/".$item_info['id']."/",110);
+                        $middle_string .= $objInfo->get_user_name_image_or_src_from_user_side($user_id ,$hotel_id=1, $name="namecard_memo.png",$extra="guest/".$item_info['id']."/",110);
                 
                         //52.63
                         $html2.="<td width=\"50%\" style=\"width:50%;height:20px;\">".$middle_string."
@@ -606,7 +606,7 @@ foreach($tblrows as $tblrow)
                       {
 								
                         $middle_string="";
-                        $middle_string .= $objInfo->get_user_name_image_or_src_from_user_side($user_id ,$hotel_id=1, $name="namecard_memo.png",$extra="guest/".$item_info['id']."/",110);
+                        $middle_string .= $objInfo->get_user_name_image_or_src_from_user_side($user_id ,$hotel_id=1, $name="namecard_memo2.png",$extra="guest/".$item_info['id']."/",110);
 						
                         //47.37
                         $html2.="<td  width=\"50%\" style=\"width:50%;height:20px;\">".$middle_string."
