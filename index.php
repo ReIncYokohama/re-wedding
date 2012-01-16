6bb53e4ed84bb32250ad4c9ec3cbceb02893261d
YAMANAKA YAMANAKA <yamanaka@re-inc.jp>
2012-01-16 12:32:45 +0900

    #340 message modify

--- index.php
+++ index.php
@@ -278,9 +278,9 @@ $(function(){
       <strong>●Windows XP ／ Vista ／ 7</strong></strong><br />
       ・Internet Explorer ７～9<br />
 ・FireFox 7／8 <br />
-・Google Chrome 14.0~ </div>
-  <div id="foot_right"><strong>●Mac OS Ｘ 10.4~</strong><br />
-    ・Safari 5.0~<br />
+・Google Chrome 14.0以上 </div>
+  <div id="foot_right"><strong>●Mac OS Ｘ 10.4以上</strong><br />
+    ・Safari 5.0以上<br />
 ・FireFox 7／8</div>
 	    <div class="clr"></div>
 	    </div>
