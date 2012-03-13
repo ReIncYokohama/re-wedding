35dd26b09c46970b59c6d17c5ed565cd0f0c0272
rei kubonaga <kubonagarei@gmail.com>
2012-03-14 02:34:00 +0900

    change browser comment

--- admin/inc/browser.php
+++ admin/inc/browser.php
@@ -2,6 +2,6 @@
 &nbsp;&middot;Internet Explorer 7~<br />
 &nbsp;&middot;FireFox 7~<br />
 &nbsp;&middot;Google Chrome 14.0~ </div>
-  <div id="foot_right"><strong>Mac OS X 10.4</strong><br />
+  <div id="foot_right"><strong>Mac OS X 10.4~</strong><br />
 &nbsp;&middot;Safari 5.0~<br />
 &nbsp;&middot;FireFox 7~</div>
