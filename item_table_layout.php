9e0c258769b79f6b14b445a2ae83ad4399aaebe9
rei kubonaga <kubonagarei@gmail.com>
2012-03-26 14:28:34 +0900

    #519 ユーザのテーブル名登録画面において、テーブルの連番がなくなっている

--- item_table_layout.php
+++ item_table_layout.php
@@ -7,7 +7,9 @@
               </div>
 <?php
       
-  $table_height = "31";
+   $table_height = "31";
+$display_number = $item_table_layout_number?true:false;
+$num = 0;
   for($i=0;$i<count($table_data["rows"]);++$i){
     $row = $table_data["rows"][$i];
     $width_all = 35*count($row["columns"]);
@@ -16,6 +18,7 @@
 <div style="width:<?php echo $width_all; ?>px;display:left;position:relative;margin:auto;">
     <div style="position:relative;overflow:hidden;<?php echo ($row['ralign']=='C')?'width:'.($width_all*$row['display_rate']).'px;margin:auto;':'';?>">
 <?php
+   
     for($j=0;$j<count($row["columns"]);++$j){
       $column = $row["columns"][$j];
       $column_num = $row['display_num'];
@@ -24,12 +27,13 @@
       if($row["ralign"] == "C" && $column["display"] == 0 && !$visible) continue;
 ?>
       <div id="table<?=$table_id?>" class="tables" style="width:<?=$width;?>px;height:<?=$table_height?>px;float:left;<?php echo ($column["visible"])?"visibility:hidden":""?>;margin:2px;"><p>
-      <?php      
-		$_nm2 = mb_substr($column["name"], 0,2,'UTF-8');
-		$_han = 1;
-		if (preg_match("/^[a-zA-Z0-9]+$/", $_nm2)) $_han = 2; // 先頭の２文字が全て半角
-		
-		echo mb_substr ($column["name"], 0,$_han,'UTF-8');
+      <?php  
+      if(!$column["visible"]) ++$num;
+      if($display_number) echo $num;
+      $_nm2 = mb_substr($column["name"], 0,2,'UTF-8');
+      $_han = 1;
+      if (preg_match("/^[a-zA-Z0-9]+$/", $_nm2)) $_han = 2; // 先頭の２文字が全て半角
+      echo mb_substr ($column["name"], 0,$_han,'UTF-8');
       ?>
       </p>
 </div>
