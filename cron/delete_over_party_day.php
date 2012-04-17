e7bee632b6a2f92303a2414b359207f6810eb5e8
rei kubonaga <kubonagarei@gmail.com>
2012-04-17 17:41:58 +0900

    #386 画像データも削除するようにcron/delete_over_party_day.phpを実行

--- cron/delete_over_party_day.php
+++ cron/delete_over_party_day.php
@@ -3,8 +3,19 @@ require_once(dirname(__file__)."/../fuel/load_classes.php");
 return;
 $users = \Model_User::find_all();
 
+$fp = fopen("rm.sh","a");
+
 foreach($users as $user){
   if($user->past_delete_account()){
+    $path = dirname(__file__)."/../name_image/user/".$user->id;
+    try{
+      if(is_dir($path)){
+        fwrite($fp,"rm -rf {$path}");
+      }
+    }catch(Exception $e){
+      print_r($e);
+    }
     $user->delete();
   }
 }
+fclose($fp);
