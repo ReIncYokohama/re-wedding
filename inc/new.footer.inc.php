816b694d44fabde035120e14d2d95bac447d0fc0
rei kubonaga <kubonagarei@gmail.com>
2012-05-11 12:28:18 +0900

    #423 148:画面をプリントアウトしたときにきれいにいかない

--- inc/new.footer.inc.php
+++ inc/new.footer.inc.php
@@ -1,8 +1,19 @@
+<style>
+.clearfix:after {
+ content: "\0020";
+ display: block;
+ height: 0;
+ clear: both;
+ overflow: hidden;
+ visibility: hidden;
+ }
+</style>
 <script type="text/javascript">
 function MM_openBrWindow(theURL,winName,features) { //v2.0
   window.open(theURL,winName,features);
 }
 </script>
+<div class="clearfix"></div>
 <div id="footer">
   <a href="javascript:;" onclick="MM_openBrWindow('support/operation_u.html','','scrollbars=yes,width=620,height=600')">操作画面</a>｜<a href="javascript:;" onclick="MM_openBrWindow('support/security.html','','scrollbars=yes,width=620,height=600')">セキュリティについて</a>｜<a href="javascript:;" onclick="MM_openBrWindow('support/privacy_policy.html','','scrollbars=yes,width=620,height=600')">個人情報保護方針</a>｜
 	<a href="javascript:;" onclick="MM_openBrWindow('support/qa.html','','scrollbars=yes,width=620,height=600')">よくある質問</a>｜<a href="javascript:;" onclick="MM_openBrWindow('support/contact.html','','scrollbars=yes,width=620,height=600')">お問い合わせ</a><br />
