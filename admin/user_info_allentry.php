4471c6a8531b6afec51472b4ff15f2e9d67693e8
rei kubonaga <kubonagarei@gmail.com>
2012-05-17 19:02:32 +0900

    引き出物のブランクへの対応

--- admin/user_info_allentry.php
+++ admin/user_info_allentry.php
@@ -633,6 +633,22 @@ function valid_user(user_id, noUpdate, count_gift, count_group, count_child) // 
 	if (valid_plan(noUpdate) == false) return false;
 	if (checkGiftForm(count_gift, noUpdate) == false) return false;
 	if (checkGroupForm(count_group, noUpdate) == false) return false;
+  for(i=1;i<=count_gift;++i){
+    name = $j("#item"+i).val();
+    if(name==""){
+      alert("ブランクのグループ記号があります");
+      $j("#item"+i).focus();
+      return false;
+    }
+  }
+  for(i=1;i<=count_group;++i){
+    name = $j("#name_group"+i).val();
+    if(name==""){
+      alert("ブランクのグループ記号があります");
+      $j("#name_group"+i).focus();
+      return false;
+    }
+  }
 
    //gaiji_check
    var return_flag = true;
@@ -1468,7 +1484,7 @@ if($updating){
 		  	$yy = 1;
 			foreach($gifts as $gift_name)
 			{
-				echo "<tr><td style='text-align:left;'><input type='text' id='item".$yy.$disp_option1."' style='padding-top:3px; padding-buttom:3px;border-style:inset;".$disp_option2.$disp_option3."' name='name_gift".$yy."' value='".$gift_name['name']."' size='30'>&nbsp;&nbsp;&nbsp;";
+				echo "<tr><td style='text-align:left;'><input type='text' id='item".$yy."' ".$disp_option1." style='padding-top:3px; padding-buttom:3px;border-style:inset;".$disp_option2.$disp_option3."' name='name_gift".$yy."' value='".$gift_name['name']."' size='30'>&nbsp;&nbsp;&nbsp;";
 				echo "<input type='hidden' name='gift_fieldId".$yy."' value='".$gift_name['id']."'></td></tr>";
 				$yy++;
 			}
