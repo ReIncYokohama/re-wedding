271d04e048fbe9c30bab9cc10715a3a3e162bfc6
rei kubonaga <kubonagarei@gmail.com>
2012-06-15 19:21:49 +0900

    Add the validation of marrige date #630 #629

--- admin/user_info_allentry.php
+++ admin/user_info_allentry.php
@@ -367,12 +367,6 @@ function valid_user(user_id, noUpdate, count_gift, count_group, count_child) // 
        document.getElementById('party_hour').focus();
        return false;
 	   }
-     if(str6 > 23)
-       {
-         alert("披露宴時間は0から23の間で入力してください");
-         document.getElementById('party_hour').focus();
-         return false;
-       }
      
      if(str6 > 22 || str6 < 7)
        {
@@ -495,8 +489,7 @@ function valid_user(user_id, noUpdate, count_gift, count_group, count_child) // 
 		document.getElementById('marriage_hour').focus();
 		return false;
 	}
-
-	if((str4!="" && Number(str4) != 0) && (str4 > 22 || str4 < 7))
+	if((str4!="") && (Number(str4) > 22 || Number(str4) < 7))
 	{
 		alert("挙式時間は7:00〜22:00の間で入力してください");
 		document.getElementById('marriage_hour').focus();
