92fa097a82826b5b8d96d470e54a00e5302ded09
rei kubonaga <kubonagarei@gmail.com>
2012-07-02 13:26:56 +0900

    Fix the notification of the messages #594

--- admin/ajax/view_user_message.php
+++ admin/ajax/view_user_message.php
@@ -1,10 +1,8 @@
 <?php
 	require_once("../inc/class.dbo.php");
-//	include_once("../inc/checklogin.inc.php");
 
 	$obj = new DBO();
-	if ($_POST['modify']==1) { // UCHIDA EDIT 11/08/19 既読をする、しないを確認
+	if ($_POST['modify']==1) {
 		$post['admin_viewed']=1;
 		$obj->UpdateData("spssp_message", $post, " id=".(int)$_POST['id']);
 	}
-?>
--- admin/message_user.php
+++ admin/message_user.php
@@ -2,6 +2,8 @@
 require_once("inc/include_class_files.php");
 include_once("inc/checklogin.inc.php");
 include_once("inc/new.header.inc.php");
+include_once("../fuel/load_classes.php");
+
 $obj = new DBO();
 $objInfo = new InformationClass();
 
@@ -19,14 +21,9 @@ if($user_id > 0) {
 	}
 
 	$modify = 0;
-	$stuff_id = $_GET['stuff_id'];
-	if ($stuff_id=="" || $stuff_id=="0") {
-		$modify = 1; // 未読を既読にする
-	}
-	$user_type = $_SESSION['user_type'];
-
-
-//echo "<script> alert('$stuff_id $where'); </script>";
+  if(!Core_Session::is_super()){
+		$modify = 1; // 未読を既読にする    
+  }
 
 	$data_per_page=5;
 	$current_page=(int)$_GET['page'];
