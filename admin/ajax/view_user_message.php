84cfe1a07f3a4a2c7352c4a3ff10c8c304121e42
rei kubonaga <kubonagarei@gmail.com>
2012-07-02 13:56:23 +0900

    Fix the notification of the messages #594

--- admin/ajax/view_user_message.php
+++ admin/ajax/view_user_message.php
@@ -4,5 +4,5 @@ include_once("../../fuel/load_classes.php");
 
 if(!Core_Session::is_super()){
   $message = Model_Message::find_by_pk($_POST["id"]);
-  $message->read(Core_Session::get_staff_id();
+  $message->read(Core_Session::get_staff_id());
 }
