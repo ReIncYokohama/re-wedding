fa23e5ec9d38286acd6c3999eb979d59e50040fb
rei kubonaga <kubonagarei@gmail.com>
2012-07-05 16:31:21 +0900

    Fix the bug of the path

--- tools/make_user_images.php
+++ tools/make_user_images.php
@@ -1,8 +1,8 @@
 <?php
-include_once(dirname(__FILE__)."../admin/inc/dbcon.inc.php");
-include_once(dirname(__FILE__)."../admin/inc/class.dbo.php");
-include_once(dirname(__FILE__)."../inc/gaiji.image.wedding.php");
-include_once(dirname(__FILE__)."../fuel/load_classes.php");
+include_once(dirname(__FILE__)."/../admin/inc/dbcon.inc.php");
+include_once(dirname(__FILE__)."/../admin/inc/class.dbo.php");
+include_once(dirname(__FILE__)."/../inc/gaiji.image.wedding.php");
+include_once(dirname(__FILE__)."/../fuel/load_classes.php");
 //fuelのパッケージがcommandの場合うまく働かないため直接指定。
 include_once(dirname(__FILE__)."/../fuel/app/classes/core/image.php");
 $_SERVER["SCRIPT_FILENAME"] = __FILE__;
@@ -26,6 +26,8 @@ foreach($users as $user){
   echo $user_row["id"].":".$user_row["man_lastname"];
   list($man_firstname_gaijis,$man_lastname_gaijis,$woman_firstname_gaijis,$woman_lastname_gaijis)
     = $user->get_gaiji_arr();
+  print_r($man_firstname_gaijis);
+  exit;
   make_user_images($user->id,$user->man_lastname,$user->man_firstname,$user->woman_lastname,
     $user->woman_firstname,$man_lastname_gaijis,$man_firstname_gaijis,$woman_lastname_gaijis,$woman_firstname_gaijis);
     //ゲストとして新郎を登録
